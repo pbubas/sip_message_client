17efc4988bb3f901329a97b3145d4b561e81cc69
Przemyslaw Bubas <bprzemys@cisco.com>
2022-11-26 10:32:33 +0100

    fixes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from core.sip_message import SipMessage,SipServer
+from core.sip_message import SipServer
 import logging
 from sys import stdout
 
@@ -7,7 +7,6 @@ LOG = logging.getLogger()
 
 if __name__ == "__main__":
     sip_server = SipServer("10.0.0.15", 5060, "sipuser", "password")
-    message = SipMessage(sip_server, "+48555222111", "test message")
+    response = sip_server.message.send("+48555222111", "test message")
 
-    response = message.send()
     if response.ok: LOG.info ('message sent successfully')
--- core/sip_message.py
+++ core/sip_message.py
@@ -1,5 +1,5 @@
+from dataclasses import dataclass
 from .sip_server_response import SipServerResponse
-from .sip_server import SipServer
 
 import socket
 import random
@@ -7,20 +7,27 @@ import string
 import uuid
 from hashlib import md5,sha256
 
+@dataclass()
+class SipServer:
+    IP : str
+    port : int
+    user : str
+    password : str
+
+    def __post_init__(self):
+        self.message = SipMessage(self)
+
 
 class SipMessage:
     def __init__(
         self,
         sip_server: SipServer,
-        destination_number: str,
-        message_text: str
     ):
-        self.to = destination_number
         self.sip_server = sip_server
-        self.message_text = message_text
 
-    def send(self, message_text: str = None):
-        self.message_text = message_text if message_text else self.message_text
+    def send(self, destination_number: str, message_text: str):
+        self.message_text = message_text
+        self.to = destination_number 
         self.my_ip = "0.0.0.0"
         self.my_port = 5060
 
@@ -46,22 +53,24 @@ class SipMessage:
 
     def _build_message(self, www_authenticate=None):
         header = dict()
-        method = f'MESSAGE sip:{self.to}@{self.sip_server.IP};transport=UDP SIP/2.0\r\n'
-        header['Via'] = f'SIP/2.0/UDP {self.my_ip}:{self.my_port};branch={self.branchid};rport\r\n'
-        header['Max-Forwards'] = f'70\r\n'
-        header['To']  = f'<sip:{self.to}@{self.sip_server.IP};transport=UDP>\r\n'
-        header['From'] = f'<sip:{self.sip_server.user}@{self.sip_server.IP};transport=UDP>;tag={self.tag}\r\n'
-        header['Call-ID'] = f'{self.call_id}\r\n'
-        header['CSeq'] = f'{self.counter} MESSAGE\r\n'
-        header['Allow'] = 'INVITE, ACK, CANCEL, BYE, NOTIFY, REFER, MESSAGE, OPTIONS, INFO, SUBSCRIBE\r\n'
-        header['Content-Type'] = 'text/plain\r\n'
+        method = f'MESSAGE sip:{self.to}@{self.sip_server.IP};transport=UDP SIP/2.0'
+        header = {
+            'Via': f'SIP/2.0/UDP {self.my_ip}:{self.my_port};branch={self.branchid};rport',
+            'Max-Forwards': f'70',
+            'To': f'<sip:{self.to}@{self.sip_server.IP};transport=UDP>',
+            'From': f'<sip:{self.sip_server.user}@{self.sip_server.IP};transport=UDP>;tag={self.tag}',
+            'Call-ID': f'{self.call_id}',
+            'CSeq': f'{self.counter} MESSAGE',
+            'Allow': 'INVITE, ACK, CANCEL, BYE, NOTIFY, REFER, MESSAGE, OPTIONS, INFO, SUBSCRIBE',
+            'Content-Type': 'text/plain',
+            'User-Agent': 'SipSmsMessage',
+            'Allow-Events': 'presence, kpml, talk ',
+            'Content-Length': f'{len(self.message_text)}'
+        }
         if www_authenticate:
             header['Authorization'] = self._build_authorization(www_authenticate)
-        header['User-Agent'] = 'SipSmsMessage\r\n'
-        header['Allow-Events'] = 'presence, kpml, talk\r\n'
-        header['Content-Length'] = f'{len(self.message_text)}\r\n'
-        request = method
-        request = method + ''.join(f'{k}: {v}' for k,v in header.items())
+        request = method + '\r\n'
+        request = request + '\r\n'.join(f'{k}: {v}' for k,v in header.items())
         request = request + '\r\n' + self.message_text
         self.method = method
         self.header = header
